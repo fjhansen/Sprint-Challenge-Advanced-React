6f644e1738b406efd1528be8252e1680aac35eb9
Frances Hansen <francesjuniperhansen@gmail.com>
2020-03-13 10:26:08 -0700

    final for mvp

---
+++ client/src/Components/__test__/PlayerCard.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import PlayerCard from './../PlayerCard';
+import { isTSAnyKeyWord } from '@babel/types'
+
+import { render } from '@testing-library/react'
+
+// it('renders a player name', () => {
+//   const { getByText } = render(<PlayerCard/>);
+//   const playerName = getByText(/Alex/i);
+
+// })
